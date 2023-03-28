41fb2493c8231956a388842c48f4002f4d1c5545
yichang1983 <85631888+yichang1983@users.noreply.github.com>
2023-03-28 17:10:26 +0100

    Add files via upload

---
+++ 221_遞歸之回顧函數返回值.py
@@ -0,0 +1,8 @@
+# 回顧函數返回值： 寫法 和返回的位置： 函數調用的位置
+
+def return_num():
+    return 100
+
+result = return_num()       # 返回的值（100）就是這個 "函數調用的位置(return_numb())" , 再用變量把它存下來。
+
+print(result)
---
+++ 222_遞歸的應用.py
@@ -0,0 +1,16 @@
+# 需求： 3以內數字累加和 3 + 2 + 1 ＝ 6
+# 6 ＝ 3 ＋ 2 以內數字累加和
+# 2以內數字累加和 ＝ 2 + 1 以內數字累加和
+# 1以內數字累加和 ＝ 1  # 出口
+
+# 遞歸特點： 函數內部自己調用自己； 必需有出口
+
+def sum_numbers(num):
+    # 2。出口
+    if num == 1:
+        return 1
+    # 1。當前數字 ＋ 當前數字-1的累加和（sum_numbers(num-1) --> 函數內部自己調用自己）
+    return num + sum_numbers(num-1)
+
+result = sum_numbers(5)
+print(result)
